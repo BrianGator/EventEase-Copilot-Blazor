acc20c7b99918be721334222058373218cdeb9bb
Brian McCarthy <briansmc@gmail.com>
2026-06-01 19:37:22 -0400

    Add registration info model

---
+++ Models/RegistrationInfo.cs
@@ -0,0 +1,10 @@
+namespace EventEase_Copilot_Blazor.Models;
+
+public class RegistrationInfo
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string ContactNumber { get; set; } = string.Empty;
+}
